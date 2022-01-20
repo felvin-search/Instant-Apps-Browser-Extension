4fb1a7517aea05f51ea7ce2728b552347033cfb3
N-Shar-ma <bablinaneh@gmail.com>
2022-01-20 12:37:18 +0530

    show search on Felvin prompt only when there is an instant app

--- content.js
+++ content.js
@@ -2,6 +2,30 @@ import React from "react";
 import { render } from "react-dom";
 import apps from "@felvin-search/apps";
 
+function preparePageForApp() {
+    const injectedApp = document.createElement("div");
+    injectedApp.id = "injected-app";
+    injectedApp.style.marginBottom = "1rem";
+    injectedApp.style.border = "1px solid grey";
+    injectedApp.style.overflow = "auto";
+
+    const felvinPrompt = document.createElement("a");
+    felvinPrompt.id = "felvin-prompt";
+    felvinPrompt.innerText = "Search on Felvin instead";
+    felvinPrompt.href = `https://felvin.com/search?q=${query}`;
+    felvinPrompt.style.display = "inline-block";
+    felvinPrompt.style.marginBottom = "1rem";
+
+    const parentDiv = document.querySelector("#rso");
+    parentDiv.insertBefore(felvinPrompt, parentDiv.firstChild);
+    parentDiv.insertBefore(injectedApp, felvinPrompt.nextSibling);
+    
+    // counter the search button shift
+    const searchBarArea = document.querySelector("#tsf");
+    const searchButton = searchBarArea.querySelector(".Tg7LZd")
+    searchButton.style.margin = 0;
+}
+
 async function renderApp(query) {
     for (const app of apps) {
       // Question: Not all apps need to return data.
@@ -10,11 +34,12 @@ async function renderApp(query) {
             const data = await app.queryToData({ query });
             // TODO: This will always render the first app
             if (!!data) {
-            render(
-                <app.Component data={data} />,
-                document.querySelector("#injected-app")
-            );
-            return;
+                preparePageForApp();
+                render(
+                    <app.Component data={data} />,
+                    document.querySelector("#injected-app")
+                );
+                return;
             }
         } catch (error) {
             console.log(`${error} in ${app}`);
@@ -22,28 +47,6 @@ async function renderApp(query) {
     }
 }
 
-const injectedApp = document.createElement("div");
-injectedApp.id = "injected-app";
-injectedApp.style.marginBottom = "1rem";
-injectedApp.style.border = "1px solid grey";
-injectedApp.style.overflow = "auto";
-
 const query = new URLSearchParams(window.location.search).get("q");
-
-const felvinPrompt = document.createElement("a");
-felvinPrompt.id = "felvin-prompt";
-felvinPrompt.innerText = "Search on Felvin instead";
-felvinPrompt.href = `https://felvin.com/search?q=${query}`;
-felvinPrompt.style.display = "inline-block";
-felvinPrompt.style.marginBottom = "1rem";
-
-const parentDiv = document.querySelector("#rso");
-parentDiv.insertBefore(felvinPrompt, parentDiv.firstChild);
-parentDiv.insertBefore(injectedApp, felvinPrompt.nextSibling);
-
-// counter the search button shift
-const searchBarArea = document.querySelector("#tsf");
-const searchButton = searchBarArea.querySelector(".Tg7LZd")
-searchButton.style.margin = 0;
 
 renderApp(query);
