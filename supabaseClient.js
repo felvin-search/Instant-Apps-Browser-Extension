5df16c30305881953fee62e135c0073136e7a482
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2022-08-21 20:03:46 +0530

    stop tracking env

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nuufqnqrpvtzirnyzmez.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dWZxbnFycHZ0emlybnl6bWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEwNzYzOTMsImV4cCI6MTk3NjY1MjM5M30.AqMVB4KdPz0nO79ZOz2FQcTRZiaqAJYif3ye6dCxcB0'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
